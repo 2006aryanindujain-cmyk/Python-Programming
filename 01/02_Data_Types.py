fe3cf64034748913b5c9e2ffd53f0c8e34d18f7c
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-20 22:46:47 +0530

    Data types in Python

---
+++ 01/02_Data_Types.py
@@ -0,0 +1,50 @@
+# integer
+print(8)
+print(1e308)
+print(1e309)
+
+
+# decimal/float
+print(8.55)
+print(1.7e308)
+print(1.7e309)
+
+
+# boolean
+print(True)
+print(False)
+
+
+# text/string
+print("Hello World")
+
+
+# complex
+print(5 + 6j)
+print(1e308 + 1e308j)
+print(1e309 + 1e309j)
+
+
+# list
+print([1, 2, 3, 4, 5])
+
+
+# tuple
+print((1, 2, 3, 4, 5))
+
+
+# sets
+print({1, 2, 3, 4, 5})
+
+
+# dictionary
+dict = {"Name": "Aryan", "Gender": "Male", "Weight": 70}
+print(dict)
+
+
+# type
+print(type(3.5))
+print(type(3))
+print(type("Aryan"))
+print(type(3 + 5j))
+print(type([1, 2, 3]))
