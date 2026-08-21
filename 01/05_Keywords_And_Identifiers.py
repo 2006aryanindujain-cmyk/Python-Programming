7abfe31f2886adf8cace6e901450ab85e7d02b0f
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-21 09:36:33 +0530

    Keywords and Identifiers in Python

---
+++ 01/05_Keywords_And_Identifiers.py
@@ -0,0 +1,29 @@
+# Keywords are the reserved words that has some specific meaning in that language.
+
+
+# Python is an interpreted language.
+
+
+# Keywords cannot be used as identifier name.
+
+
+# Identifiers are the names given by the programmer to the variable, object and functions.
+
+
+# identifiers cannot start with a digit.
+name1 = "Aryan"
+print(name1)
+
+
+# We can use onle '_' special character in naming identifiers.
+_ = "Aryan"
+print(_)
+
+
+# Identifiers cannot be keywords.
+
+
+# Identifiers only contain digits, alphabets, underscore(_).
+
+
+# Identifiers and keywords are case-sensitive.
