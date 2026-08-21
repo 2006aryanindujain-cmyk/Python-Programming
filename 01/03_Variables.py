12caa56db568211ebc6c3d3e0b90f7985be68bb0
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-21 09:05:10 +0530

    Variables in Python

---
+++ 01/03_Variables.py
@@ -0,0 +1,41 @@
+name = "Aryan"
+print(name)
+a = 5
+b = 6
+print(a + b)
+
+
+# Dynamic Typing
+c = 5
+
+
+# static typing
+# int a=5
+
+
+# Python supports dynamic typing
+
+
+# dynamic binding
+d = 5
+print(a)
+d = "Aryan"
+print(d)
+
+
+# static binding
+# int a = 5;
+# printf("%d",a);
+# string a ="Aryan";
+# printf("%s",a);
+
+
+# Python supports dynamic binding
+
+
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+
+a = b = c = 5
+print(a, b, c)
