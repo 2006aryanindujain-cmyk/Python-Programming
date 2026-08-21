9fee44100551cbeea37374b3f92e64f907352c4f
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-21 10:02:03 +0530

    User input in Python

---
+++ 01/06_User_Input.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter second number:"))
+res = a + b
+print(res)
+print("Sum is", a + b)
+input()
+
+
+# Python always take input from user automatically in string but we can change the data type of user input using typecasting.
