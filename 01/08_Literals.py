2bdafd524e0f22b2a37468fdff64aad4dec88999
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-21 15:11:59 +0530

    Literals in Python

---
+++ 01/08_Literals.py
@@ -0,0 +1,43 @@
+a = 0b1010  # binary literal
+b = 100  # decimal literal
+c = 0o310  # octal literal
+d = 0x12C  # hexadecimal literal
+print(a, b, c, d)
+
+
+# float literal
+float_1 = 10.55
+float_2 = 1.5e2
+float_3 = 1.5e-3
+print(float_1, float_2, float_3)
+
+
+# complex literal
+x = 3.14
+print(x, x.real, x.imag)
+
+
+# strings literal
+string = "This is Python"
+strings = "This is Python"
+char = "C"
+multiline_str = """This is a multiline string with more than one line code."""
+unicode = "\U0001f600\U0001f606\U0001f923"
+raw_str = r"raw \n string"
+print(string)
+print(strings)
+print(char)
+print(multiline_str)
+print(unicode)
+print(raw_str)
+
+
+# boolean literal
+a = True + 4
+b = False + 10
+print("a:", a)
+print("b:", b)
+
+
+# none literal
+a = print(a)
