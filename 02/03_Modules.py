da66564732e17750cab2e45cb16047b9536be183
Aryan Jain <2006aryanindujain@gmail.com>
2026-08-26 12:01:46 +0530

    Modules in Python

---
+++ 02/03_Modules.py
@@ -0,0 +1,32 @@
+# A module in Python is a file containing Python code—such as functions, classes, and
+# variables — that can be imported and reused in other Python programs.
+
+
+# math module
+import math
+
+print(math.factorial(5))
+print(math.floor(6.8))
+print(math.sqrt(625))
+
+
+# keyword module
+import keyword
+
+print(keyword.kwlist)
+
+
+# random module
+import random
+
+print(random.randint(1, 100))
+
+
+# datetime module
+import datetime
+
+print(datetime.datetime.now())
+
+
+# all modules in python
+help("modules")
