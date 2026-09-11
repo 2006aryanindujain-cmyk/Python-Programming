e9c0cc445bffbc05d9e7b954ae86af71a09ec812
Aryan Jain <2006aryanindujain@gmail.com>
2026-09-11 10:44:29 +0530

    Adding items to a list

---
+++ 04/03_Adding_Items_to_a_List.py
@@ -0,0 +1,17 @@
+# append :-
+L = [1, 2, 3, 4, 5]
+L.append(9)
+L.append([6, 7, 8])
+print(L)
+
+
+# extend :-
+l = [1, 2, 3, 4, 5]
+l.extend([6, 7, 8])
+l.extend("Delhi")
+print(l)
+
+# insert :-
+p = [1, 2, 3, 4, 5]
+p.insert(1, 100)
+print(p)
